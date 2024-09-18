2b9ea6464ece2502d10e648ddbe99aafae0c34b0
EmilioIzquierdo <cucusneitor@hotmail.com>
2024-09-17 20:37:04 -0600

    Add files via upload

---
+++ DiaSoleado.js
@@ -0,0 +1,19 @@
+background(186, 145, 20); // wooden table
+ellipse(200, 200, 350, 350); // plate
+ellipse(200, 200, 300, 300); 
+
+// pizza
+fill(186, 172, 67);
+triangle(291, 123, 181, 294, 77, 126);
+// pepperonis
+fill(255, 0, 0);
+stroke(255, 0, 0);
+ellipse(200, 221, 30, 30);
+ellipse(158, 221, 30, 30); 
+ellipse(222, 179, 34, 30); 
+ellipse(144, 179, 34, 30);
+ellipse(181, 179, 34, 30);
+ellipse(144, 148, 34, 30);
+ellipse(181, 148, 34, 30);
+ellipse(112, 149, 34, 30);
+ellipse(240, 144, 34, 30);
---
+++ que hay de cenar.js
@@ -0,0 +1,14 @@
+background(0, 213, 255);
+// The ground
+fill(66, 237, 94);
+rect(0, 300, 400, 100);  
+
+// The sun
+fill(204, 240, 22);
+ellipse(80, 64, 100, 100);  
+
+// The snowman
+fill(255, 255, 255);
+ellipse(200, 300, 150, 150);
+ellipse(200, 200, 100, 100);
+ellipse(200, 120, 75, 75);
